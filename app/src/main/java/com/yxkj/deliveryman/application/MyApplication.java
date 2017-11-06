8fe54f64d9de8ca08cc6abb426bae21903ab4037
hhe <hhecoder@126.com>
2017-11-06 11:05:12 +0800

    feat 继承极光推送 appkey还未配置

--- app/src/main/java/com/yxkj/deliveryman/application/MyApplication.java
+++ app/src/main/java/com/yxkj/deliveryman/application/MyApplication.java
@@ -3,18 +3,15 @@ package com.yxkj.deliveryman.application;
 import android.app.Application;
 import android.content.Context;
 
+import cn.jpush.android.api.JPushInterface;
+
 /**
  *
+ * @author hhe
  */
 
 public class MyApplication extends Application {
     private static MyApplication myApplication;
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        myApplication = this;
-    }
 
     public static MyApplication getMyApplication() {
         return myApplication;
@@ -23,4 +20,20 @@ public class MyApplication extends Application {
     public static Context getAppContext() {
         return getMyApplication().getApplicationContext();
     }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        myApplication = this;
+
+        initJpush();
+
+    }
+
+    private void initJpush() {
+        //先后顺序不要变,避免出现部分日志没打印的情况
+        JPushInterface.setDebugMode(true);
+        JPushInterface.init(this);
+    }
+
 }
